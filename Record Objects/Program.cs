38541b48704c43169dae41d5c552b5769309c138
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2019-05-01 19:58:57 -0600

    Set up initial classes

--- Record Objects/Program.cs
+++ Record Objects/Program.cs
@@ -19,4 +19,118 @@ namespace Record_Objects
             Application.Run(new Form1());
         }
     }
+
+    class Employee
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private string zipCode;
+
+        public Employee() { }
+
+        public string GetName()
+        {
+            return $"{firstName} {lastName}";
+        }
+
+        public string GetAddress()
+        {
+            return $"{address}, {city}, {state}, {zipCode}";
+        }
+
+        public void SetName(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void SetAddress(string address, string city, string state, string zipCode)
+        {
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+        }
+    }
+
+    class Manager : Employee
+    {
+        private string costCenter;
+        private string supervisor;
+
+        public string GetCostCenter()
+        {
+            return costCenter;
+        }
+
+        public string GetSupervisor()
+        {
+            return supervisor;
+        }
+
+        public string GetDevType()
+        {
+            return "N/A";
+        }
+
+        public string GetTaxType()
+        {
+            return "N/A";
+        }
+
+        public void SetCostCenter(string costCenter)
+        {
+            this.costCenter = costCenter;
+        }
+
+        public void SetSupervisor(string supervisor)
+        {
+            this.supervisor = supervisor;
+        }
+    }
+
+    class Developer : Employee
+    {
+        private string developerType;
+        private string supervisor;
+        private string taxType;
+
+        public string GetDevType()
+        {
+            return developerType;
+        }
+
+        public string GetSupervisor()
+        {
+            return supervisor;
+        }
+
+        public string GetTaxType()
+        {
+            return taxType;
+        }
+
+        public string GetCostCenter()
+        {
+            return "0";
+        }
+
+        public void SetDevType(string developerType)
+        {
+            this.developerType = developerType;
+        }
+
+        public void SetSupervisor(string supervisor)
+        {
+            this.supervisor = supervisor;
+        }
+
+        public void SetTaxType(string taxType)
+        {
+            this.taxType = taxType;
+        }
+    }
 }
